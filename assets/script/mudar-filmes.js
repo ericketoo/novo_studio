816f5249e4dd61abf6f614bba00302d16ab07fc3
ericketoo <egacgamer@gmail.com>
2023-07-25 09:30:14 -0300

    testes que n sei

--- assets/script/mudar-filmes.js
+++ assets/script/mudar-filmes.js
@@ -4,18 +4,21 @@ document.addEventListener("DOMContentLoaded", function() {
       {
         imagem: "../img/diretores-index/DIRETORES-INDEX-IMG4.png",
         texto: "Texto 1",
-        film: "como treinar drgão"
+        film: "como treinar drgão",
+        link: "page1.html"
       },
       {
-        imagem: "../assets/img/diretores-index-img1.png",
+        imagem: "../img/diretores-index/DIRETORES-INDEX-IMG6.png",
         texto: "Texto 2",
-        film: "amigo totoro"
+        film: "amigo totoro",
+        link: "page2.html"
       },
       {
-        imagem: "../assets/img/diretores-index-img2.png",
+        imagem: "../img/diretores-index/DIRETORES-INDEX-IMG5.png",
         texto: "Texto 3",
-        film: "batata"
-      },
+        film: "batata",
+        link: "page3.html"
+      },/*
       {
         imagem: "caminho-da-imagem-3.jpg",
         texto: "Texto 4",
@@ -40,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function() {
         imagem: "caminho-da-imagem-3.jpg",
         texto: "Texto 8",
         film:"macarakwfjb"
-      }
+      }/*/
     ];
   
     // Função para obter uma lista aleatória sem repetição
@@ -64,16 +67,21 @@ document.addEventListener("DOMContentLoaded", function() {
       const elementosImagem = document.querySelectorAll(".img-film");
       const elementosTexto = document.querySelectorAll(".info-film");
       const elementosfilm = document.querySelectorAll(".name-film");
-  
+      const elementosLink = document.querySelectorAll(".link-film");
+      
       elementosImagem.forEach(function(elemento, indice) {
         elemento.src = conteudosAleatorios[indice].imagem;
       });
-  
       elementosTexto.forEach(function(elemento, indice) {
         elemento.textContent = conteudosAleatorios[indice].texto;
       });
       elementosfilm.forEach(function(elemento, indice) {
         elemento.textContent = conteudosAleatorios[indice].film;
+      });
+      elementosLink.forEach(function(elemento, indice) {
+        if (conteudosAleatorios[indice] && conteudosAleatorios[indice].link) {
+          elemento.setAttribute("href", conteudosAleatorios[indice].link);
+        }
       });
     }
   
